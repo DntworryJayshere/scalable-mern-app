da63f5492ac489b854c0a131aa11374b56c5dd5a
Jay Rodriguez <jared.m.rodriguez@gmail.com>
2021-03-11 17:35:53 -0600

    frontend file dump

--- client/next.config.js
+++ client/next.config.js
@@ -1,2 +1,9 @@
 const withCSS = require('@zeit/next-css');
-module.exports = withCSS({});
+module.exports = withCSS({
+	publicRuntimeConfig: {
+		APP_NAME: 'scalable-mern-app',
+		API: 'http://localhost:8000/api',
+		PRODUCTION: false,
+		DOMAIN: 'http://localhost:3000',
+	},
+});
