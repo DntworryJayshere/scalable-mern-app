f6ae7f384f4673dfe7fdb5a24e56c772d3cce005
Jay Rodriguez <jared.m.rodriguez@gmail.com>
2021-03-17 12:32:07 -0500

    clean start for front end debugging

--- server.js
+++ server.js
@@ -8,16 +8,23 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-//init middleware
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json({ limit: '5mb', extended: false }));
-app.use(morgan('dev'));
-app.use(routes);
-
-// app.use(cors());
-app.use(cors({ origin: process.env.CLIENT_URL }));
-
 // db
 connectDB();
+
+//init middleware
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '5mb', extended: false }));
+
+//import & config cors
+const corsOptions = {
+	origin: process.env.CLIENT_URL,
+	credentials: true, //access-control-allow-credentials:true
+	optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+//import routes
+app.use(routes);
 
 app.listen(PORT, () => console.log('Server started on port' + PORT));
